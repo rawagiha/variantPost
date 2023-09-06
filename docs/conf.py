512568b03a5f34480358a7d7f4dbb74febc386da
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-09-06 16:24:50 -0500

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -116,6 +116,7 @@ except:
     subprocess.check_call(
         ["python", "-m", "pip", "install", pkg_name], stdout=subprocess.DEVNULL
     )
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
