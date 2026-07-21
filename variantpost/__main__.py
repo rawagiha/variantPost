036dbde31421e9d9604bde0d0a092e881f12ab2c
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2026-07-21 16:19:59 -0500

    aho

--- variantpost/__main__.py
+++ variantpost/__main__.py
@@ -1,15 +1,196 @@
 #!/usr/bin/env python3
 import os
 import sys
+import math
 import pysam
 import argparse
 import tempfile
 import pandas as pd
 import multiprocessing as mp
-from collections import namedtuple
+from scipy.stats import fisher_exact
+from collections import namedtuple, OrderedDict
+
 
 from .variant import Variant
 from .variantalignment import VariantAlignment
+
+KEYS_83 = (
+    "1:Del:C:0",
+    "1:Del:C:1",
+    "1:Del:C:2",
+    "1:Del:C:3",
+    "1:Del:C:4",
+    "1:Del:C:5",
+    "1:Del:T:0",
+    "1:Del:T:1",
+    "1:Del:T:2",
+    "1:Del:T:3",
+    "1:Del:T:4",
+    "1:Del:T:5",
+    "1:Ins:C:0",
+    "1:Ins:C:1",
+    "1:Ins:C:2",
+    "1:Ins:C:3",
+    "1:Ins:C:4",
+    "1:Ins:C:5",
+    "1:Ins:T:0",
+    "1:Ins:T:1",
+    "1:Ins:T:2",
+    "1:Ins:T:3",
+    "1:Ins:T:4",
+    "1:Ins:T:5",
+    "2:Del:M:1",
+    "2:Del:R:0",
+    "2:Del:R:1",
+    "2:Del:R:2",
+    "2:Del:R:3",
+    "2:Del:R:4",
+    "2:Del:R:5",
+    "2:Ins:R:0",
+    "2:Ins:R:1",
+    "2:Ins:R:2",
+    "2:Ins:R:3",
+    "2:Ins:R:4",
+    "2:Ins:R:5",
+    "3:Del:M:1",
+    "3:Del:M:2",
+    "3:Del:R:0",
+    "3:Del:R:1",
+    "3:Del:R:2",
+    "3:Del:R:3",
+    "3:Del:R:4",
+    "3:Del:R:5",
+    "3:Ins:R:0",
+    "3:Ins:R:1",
+    "3:Ins:R:2",
+    "3:Ins:R:3",
+    "3:Ins:R:4",
+    "3:Ins:R:5",
+    "4:Del:M:1",
+    "4:Del:M:2",
+    "4:Del:M:3",
+    "4:Del:R:0",
+    "4:Del:R:1",
+    "4:Del:R:2",
+    "4:Del:R:3",
+    "4:Del:R:4",
+    "4:Del:R:5",
+    "4:Ins:R:0",
+    "4:Ins:R:1",
+    "4:Ins:R:2",
+    "4:Ins:R:3",
+    "4:Ins:R:4",
+    "4:Ins:R:5",
+    "5:Del:M:1",
+    "5:Del:M:2",
+    "5:Del:M:3",
+    "5:Del:M:4",
+    "5:Del:M:5",
+    "5:Del:R:0",
+    "5:Del:R:1",
+    "5:Del:R:2",
+    "5:Del:R:3",
+    "5:Del:R:4",
+    "5:Del:R:5",
+    "5:Ins:R:0",
+    "5:Ins:R:1",
+    "5:Ins:R:2",
+    "5:Ins:R:3",
+    "5:Ins:R:4",
+    "5:Ins:R:5",
+)
+
+KEYS_89 = (
+    "A[Ins(C):R0]A",
+    "A[Ins(C):R0]T",
+    "Ins(C):R(0,3)",
+    "Ins(C):R(4,6)",
+    "Ins(C):R(7,9)",
+    "A[Ins(T):R(0,4)]A",
+    "A[Ins(T):R(0,4)]C",
+    "A[Ins(T):R(0,4)]G",
+    "C[Ins(T):R(0,4)]A",
+    "C[Ins(T):R(0,4)]C",
+    "C[Ins(T):R(0,4)]G",
+    "G[Ins(T):R(0,4)]A",
+    "G[Ins(T):R(0,4)]C",
+    "G[Ins(T):R(0,4)]G",
+    "A[Ins(T):R(5,7)]A",
+    "A[Ins(T):R(5,7)]C",
+    "A[Ins(T):R(5,7)]G",
+    "C[Ins(T):R(5,7)]A",
+    "C[Ins(T):R(5,7)]C",
+    "C[Ins(T):R(5,7)]G",
+    "G[Ins(T):R(5,7)]A",
+    "G[Ins(T):R(5,7)]C",
+    "G[Ins(T):R(5,7)]G",
+    "A[Ins(T):R(8,9)]A",
+    "A[Ins(T):R(8,9)]C",
+    "A[Ins(T):R(8,9)]G",
+    "C[Ins(T):R(8,9)]A",
+    "C[Ins(T):R(8,9)]C",
+    "C[Ins(T):R(8,9)]G",
+    "G[Ins(T):R(8,9)]A",
+    "G[Ins(T):R(8,9)]C",
+    "G[Ins(T):R(8,9)]G",
+    "Ins(2,4):R0",
+    "Ins(5,):R0",
+    "Ins(2,4):R1",
+    "Ins(5,):R1",
+    "Ins(2,):R(2,4)",
+    "Ins(2,):R(5,9)",
+    "[Del(C):R1]A",
+    "[Del(C):R1]T",
+    "[Del(C):R2]A",
+    "[Del(C):R2]T",
+    "[Del(C):R3]A",
+    "[Del(C):R3]T",
+    "[Del(C):R(4,5)]A",
+    "[Del(C):R(4,5)]T",
+    "[Del(C):R(1,5)]G",
+    "Del(C):R(6,9)",
+    "A[Del(T):R(1,4)]A",
+    "A[Del(T):R(1,4)]C",
+    "A[Del(T):R(1,4)]G",
+    "C[Del(T):R(1,4)]A",
+    "C[Del(T):R(1,4)]C",
+    "C[Del(T):R(1,4)]G",
+    "G[Del(T):R(1,4)]A",
+    "G[Del(T):R(1,4)]C",
+    "G[Del(T):R(1,4)]G",
+    "A[Del(T):R(5,7)]A",
+    "A[Del(T):R(5,7)]C",
+    "A[Del(T):R(5,7)]G",
+    "C[Del(T):R(5,7)]A",
+    "C[Del(T):R(5,7)]C",
+    "C[Del(T):R(5,7)]G",
+    "G[Del(T):R(5,7)]A",
+    "G[Del(T):R(5,7)]C",
+    "G[Del(T):R(5,7)]G",
+    "A[Del(T):R(8,9)]A",
+    "A[Del(T):R(8,9)]C",
+    "A[Del(T):R(8,9)]G",
+    "C[Del(T):R(8,9)]A",
+    "C[Del(T):R(8,9)]C",
+    "C[Del(T):R(8,9)]G",
+    "G[Del(T):R(8,9)]A",
+    "G[Del(T):R(8,9)]C",
+    "G[Del(T):R(8,9)]G",
+    "Del(2,4):R1",
+    "Del(5,):R1",
+    "Del(2,8):U(1,2):R(2,4)",
+    "Del(2,):U(1,2):R(5,9)",
+    "Del(3,):U(3,):R2",
+    "Del(3,):U(3,):R(3,9)",
+    "Del(2,5):M1",
+    "Del(3,5):M2",
+    "Del(4,5):M(3,4)",
+    "Del(6,):M1",
+    "Del(6,):M2",
+    "Del(6,):M3",
+    "Del(6,):M(4,)",
+    "Complex",
+)
 
 ALLOWED_BASES = set("ATCG")
 _worker_tumor_bam = None
@@ -23,14 +204,18 @@ Result = namedtuple(
         "ComplexPos",
         "ComplexRef",
         "ComplexAlt",
-        "TumorSuppotingCountFw",
-        "TumorSuppotingCountRv",
-        "TumorNonSuppotingCountFw",
-        "TumorNonSuppotingCountRv",
-        "NormalSuppotingCountFw",
-        "NormalSuppotingCountRv",
-        "NormalNonSuppotingCountFw",
-        "NormalNonSuppotingCountRv",
+        "TumorSupportingCountFw",
+        "TumorSupportingCountRv",
+        "TumorNonSupportingCountFw",
+        "TumorNonSupportingCountRv",
+        "TumorUniqueSupportingCount",
+        "TumorUniqueNonSupportingCount",
+        "NormalSupportingCountFw",
+        "NormalSupportingCountRv",
+        "NormalNonSupportingCountFw",
+        "NormalNonSupportingCountRv",
+        "NormalUniqueSupportingCount",
+        "NormalUniqueNonSupportingCount",
         "RefIndelChannelCOSMIC83",
         "RefIndelChannel89",
         "PersonalIndelChannelCOSMIC83",
@@ -38,10 +223,12 @@ Result = namedtuple(
     ],
 )
 
+
 def validate_file(path, label):
     if not os.path.exists(path):
         sys.stderr.write(f"Error: {label} file not found: {path}\n")
         sys.exit(1)
+
 
 def get_chrom_order(fasta_path, df_chroms):
     try:
@@ -162,14 +349,18 @@ def process_single_row(row):
         ComplexPos=valn.variant.cpos,
         ComplexRef=valn.variant.cref,
         ComplexAlt=valn.variant.calt,
-        TumorSuppotingCountFw=tumor_cnt.s_fw,
-        TumorSuppotingCountRv=tumor_cnt.s_rv,
-        TumorNonSuppotingCountFw=tumor_cnt.n_fw,
-        TumorNonSuppotingCountRv=tumor_cnt.n_rv,
-        NormalSuppotingCountFw=normal_cnt.s_fw,
-        NormalSuppotingCountRv=normal_cnt.s_rv,
-        NormalNonSuppotingCountFw=normal_cnt.n_fw,
-        NormalNonSuppotingCountRv=normal_cnt.n_rv,
+        TumorSupportingCountFw=tumor_cnt.s_fw,
+        TumorSupportingCountRv=tumor_cnt.s_rv,
+        TumorNonSupportingCountFw=tumor_cnt.n_fw,
+        TumorNonSupportingCountRv=tumor_cnt.n_rv,
+        TumorUniqueSupportingCount=tumor_cnt.s,
+        TumorUniqueNonSupportingCount=tumor_cnt.n,
+        NormalSupportingCountFw=normal_cnt.s_fw,
+        NormalSupportingCountRv=normal_cnt.s_rv,
+        NormalNonSupportingCountFw=normal_cnt.n_fw,
+        NormalNonSupportingCountRv=normal_cnt.n_rv,
+        NormalUniqueSupportingCount=normal_cnt.s,
+        NormalUniqueNonSupportingCount=normal_cnt.n,
         RefIndelChannelCOSMIC83=ref_txn_cosmic,
         RefIndelChannel89=ref_txn_89,
         PersonalIndelChannelCOSMIC83=personal_txn_cosmic,
@@ -188,8 +379,8 @@ def process_chunk(df_chunk):
 def parallel_process_dataframe(df, tumor_bam, normal_bam, fasta, num_workers=None):
     if df.empty:
         return [], []
-    
-    # df is re-indexed with drop 
+
+    # df is re-indexed with drop
 
     if num_workers is None:
         num_workers = min(8, max(1, mp.cpu_count() - 1))
@@ -222,6 +413,7 @@ def process_chunk_stream(df_chunk):
 
     return chunk_results
 
+
 def get_chrom_order(fasta_path, df_chroms):
     try:
         with pysam.FastaFile(fasta_path) as fa:
@@ -239,6 +431,74 @@ def get_chrom_order(fasta_path, df_chroms):
 
     return ordered_chroms
 
+
+def filter_row(
+    row,
+    min_strand_bias_read_count,
+    strand_bias_thresh,
+    max_normal_vaf,
+    min_enrichment_thresh,
+    pval_thresh,
+):
+    ref_fw = row["TumorNonSupportingCountFw"]
+    ref_rv = row["TumorNonSupportingCountRv"]
+    alt_fw = row["TumorSupportingCountFw"]
+    alt_rv = row["TumorSupportingCountRv"]
+
+    t_ref = row["TumorUniqueNonSupportingCount"]
+    t_alt = row["TumorUniqueSupportingCount"]
+    n_ref = row["NormalUniqueNonSupportingCount"]
+    n_alt = row["NormalUniqueSupportingCount"]
+
+    filters = []
+    # Strand bias odds ratio filter
+    sor = 0.0
+    if t_alt > min_strand_bias_read_count:
+        rf, rr = ref_fw + 1, ref_rv + 1
+        af, ar = alt_fw + 1, alt_rv + 1
+
+        r_ref = max(rf / rr, rr / rf)
+        r_alt = max(af / ar, ar / af)
+
+        ref_alt_ratio = (af * rr) / (ar * rf)
+
+        if ref_alt_ratio < 1.0:
+            ref_alt_ratio = 1.0 / ref_alt_ratio
+
+        try:
+            sor = math.log(r_ref * r_alt) + math.log(ref_alt_ratio)
+        except ValueError:
+            sor = 0.0
+
+        sor = round(sor, 4)
+
+    if sor >= strand_bias_thresh:
+        filters.append("StrandBias")
+
+    t_vaf = t_alt / (t_ref + t_alt) if (t_ref + t_alt) > 0 else 0
+    n_vaf = n_alt / (n_ref + n_alt) if (n_ref + n_alt) > 0 else 0
+
+    # Not detected
+    if t_vaf == 0:
+        filters.append("FailedToDetect")
+
+    # Normal VAF > 0
+    if n_vaf > 0:
+        if n_vaf >= max_normal_vaf:
+            filters.append("PossibleGermline")
+
+        if (t_vaf / n_vaf) < min_enrichment_thresh:
+            filters.append("WeakEnrichmentInTumor")
+
+        _, p_val = fisher_exact([[t_alt, t_ref], [n_alt, n_ref]], alternative="greater")
+        if p_val > pval_thresh:
+            filters.append("WeakStatisticalEvidence")
+
+    filter_str = "PASS" if not filters else ";".join(filters)
+
+    return t_vaf, n_vaf, sor, filter_str
+
+
 def personalizer(args):
     tumor_bam = args.tumor_bam
     normal_bam = args.normal_bam
@@ -246,9 +506,9 @@ def personalizer(args):
     output = args.output
     vcf_list = args.vcf
     processes = args.processes
-    
+
     filter_sets = {f.strip() for f in args.filters.split(";") if f.strip()}
-    
+
     df, chrom_order = extract_indels_to_dataframe(vcf_list, filter_sets, reference)
     if df.empty:
         print("No variants to process.", file=sys.stderr)
@@ -256,13 +516,13 @@ def personalizer(args):
 
     num_workers = processes
     chunks = [df[i::num_workers] for i in range(num_workers)]
-    
+
     header_written = False
-    
+
     with tempfile.TemporaryDirectory(prefix="indelinside_") as tmp_dir:
         tmp_output = os.path.join(tmp_dir, "raw_metrics.tsv")
         out_f = open(tmp_output, "w", encoding="utf-8")
-        
+
         try:
             with mp.Pool(
                 processes=num_workers,
@@ -287,33 +547,123 @@ def personalizer(args):
 
         finally:
             out_f.close()
-    
+
         dfo = pd.read_csv(tmp_output, sep="\t")
 
         chrom_category = pd.CategoricalDtype(categories=chrom_order, ordered=True)
         dfo["CHROM"] = dfo["CHROM"].astype(chrom_category)
         dfo.sort_values(by=["CHROM", "POS"], inplace=True)
+
+        (
+            dfo["TumorVAF"],
+            dfo["NormalVAF"],
+            dfo["StrandBias"],
+            dfo["FILTER"],
+        ) = zip(
+            *dfo.apply(
+                filter_row,
+                min_strand_bias_read_count=args.min_strand_bias_read_count,
+                strand_bias_thresh=args.strand_bias_thresh,
+                max_normal_vaf=args.max_normal_vaf,
+                min_enrichment_thresh=args.tumor_normal_vaf_ratio_thresh,
+                pval_thresh=args.fisher_exact_pval_thresh,
+                axis=1,
+            )
+        )
+
         dfo.to_csv(output, sep="\t", index=False)
+
+
+def mut_tbl(args):
+    df = pd.read_csv(args.input, sep="\t")
+
+    consensus_level = args.consensus_level
+    if consensus_level >= 1:
+        df = df[df["SUPPORT_COUNT"] >= consensus_level]
+    elif consensus_level == -1:
+        df = df[df["SUPPORT_COUNT"] == consensus_level]
+    else:
+        raise ValueError(f"Invalide consensus level: {consensus_level}")
+
+    df.reset_index(drop=True, inplace=True)
+
+    ref_83 = OrderedDict((key, 0) for key in KEYS_83)
+    ref_83_cnt = df["RefIndelChannelCOSMIC83"].value_counts()
+    for key, cnt in ref_83_cnt.items():
+        if key in KEYS_83:
+            ref_83[key] = cnt
+
+    personal_83 = OrderedDict((key, 0) for key in KEYS_83)
+    personal_83_cnt = df["PersonalIndelChannelCOSMIC83"].value_counts()
+    for key, cnt in personal_83_cnt.items():
+        if key in KEYS_83:
+            personal_83[key] = cnt
+
+    data_83 = []
+    for key in KEYS_83:
+        d = {
+            "MutationType": key,
+            f"{args.sample}": ref_83[key],
+            f"{args.sample}_personalized": personal_83[key],
+        }
+        data_83.append(d)
+
+    df_83 = pd.DataFrame(data_83)
+    df_83.to_csv(f"{args.sample}_indel_83_matrix.txt", sep="\t", index=False)
+
+    ref_89 = OrderedDict((key, 0) for key in KEYS_89)
+    ref_89_cnt = df["RefIndelChannel89"].value_counts()
+    for key, cnt in ref_89_cnt.items():
+        if key in KEYS_89:
+            ref_89[key] = cnt
+
+    personal_89 = OrderedDict((key, 0) for key in KEYS_89)
+    personal_89_cnt = df["PersonalIndelChannel89"].value_counts()
+    for key, cnt in personal_89_cnt.items():
+        if key in KEYS_89:
+            personal_89[key] = cnt
+
+    data_89 = []
+    for key in KEYS_89:
+        d = {
+            "MutationType": key,
+            f"{args.sample}": ref_89[key],
+            f"{args.sample}_personalized": personal_89[key],
+        }
+        data_89.append(d)
+
+    df_89 = pd.DataFrame(data_89)
+    df_89.to_csv(f"{args.sample}_indel_89_matrix.txt", sep="\t", index=False)
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         prog="indelinside",
     )
 
-    subparsers = parser.add_subparsers(dest="subcommand", required=True, help="sub-command help")
+    subparsers = parser.add_subparsers(
+        dest="subcommand", required=True, help="sub-command help"
+    )
 
-    parser_personal = subparsers.add_parser("personalize", help="reanalyze somatic indels on locally personalized reference genome")
-    
+    parser_personal = subparsers.add_parser(
+        "personalize",
+        help="reanalyze somatic indels on locally personalized reference genome",
+    )
+
     parser_personal.add_argument("-t", "--tumor_bam", help="path to tumor BAM")
     parser_personal.add_argument("-n", "--normal_bam", help="path to normal BAM")
-    parser_personal.add_argument("-r", "--reference", help="path to reference Fasta file")
-    parser_personal.add_argument("-o", "--output", type=str, help="path to output TSV file")
+    parser_personal.add_argument(
+        "-r", "--reference", help="path to reference Fasta file"
+    )
+    parser_personal.add_argument(
+        "-o", "--output", type=str, help="path to output TSV file"
+    )
 
-    #vcf_group = parser_personal.add_mutually_exclusive_group(required=True)
+    # vcf_group = parser_personal.add_mutually_exclusive_group(required=True)
     parser_personal.add_argument(
         "-v", "--vcf", nargs="+", help="path(s) to VCF(s) (space-separated)"
     )
-    #vcf_group.add_argument("--vcf_list", help="text file containing paths to VCF")
+    # vcf_group.add_argument("--vcf_list", help="text file containing paths to VCF")
 
     parser_personal.add_argument(
         "--filters",
@@ -330,15 +680,60 @@ def parse_arguments():
         help="Number of parallel worker processes (default: 4)",
     )
 
+    parser_personal.add_argument("--max_normal_vaf", type=float, default=0.1, help="aa")
+    parser_personal.add_argument(
+        "--tumor_normal_vaf_ratio_thresh", type=float, default=5.0, help="aa"
+    )
+    parser_personal.add_argument(
+        "--strand_bias_thresh", type=float, default=7.0, help="applied to count > 1"
+    )
+    parser_personal.add_argument(
+        "--min_strand_bias_read_count", type=int, default=3, help="aa"
+    )
+    parser_personal.add_argument(
+        "--fisher_exact_pval_thresh", type=float, default=0.05, help="aa"
+    )
     parser_personal.set_defaults(handler=personalizer)
 
-    
+    parser_table = subparsers.add_parser(
+        "matrix", help="prepare mutation matrix for indel signature analysis"
+    )
+    parser_table.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        help="path to the output TSV file from personalize subcommand",
+    )
+    parser_table.add_argument("-s", "--sample", type=str, help="aa")
+    parser_table.add_argument("-c", "--consensus_level", type=int, help="aa")
+    parser_table.add_argument(
+        "--filters",
+        type=str,
+        default="PASS",
+        help="Semicolon-separated list of accepted FILTER values (default: PASS). Use 'all' or '.' to allow any.",
+    )
+
+    parser_table.set_defaults(handler=mut_tbl)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+
+    if len(sys.argv) == 2 and sys.argv[1] in ["personalize", "matrix"]:
+        if sys.argv[1] == "personalize":
+            parser_personal.print_help()
+        elif sys.argv[1] == "matrix":
+            parser_table.print_help()
+        sys.exit(0)
+
     return parser.parse_args()
- 
+
+
 def main():
     args = parse_arguments()
 
     args.handler(args)
+
 
 if __name__ == "__main__":
     main()
